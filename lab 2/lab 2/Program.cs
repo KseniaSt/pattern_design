29b1d5c3f1a499b40bfd38166dcdc98c2cedd3d4
Ksenia <Ksenia@DESKTOP-FPNQ7IM>
2017-11-29 21:15:16 +0200

    Testing

--- lab 2/lab 2/Program.cs
+++ lab 2/lab 2/Program.cs
@@ -10,6 +10,32 @@ namespace lab_2
     {
         static void Main(string[] args)
         {
+            Component fileSystem = new Directory("Файловая система");
+
+            Component diskD = new Directory("Диск D");
+
+            Component jpgFile = new File("Photo.jpg");
+            Component docFile = new File("Document.docx");
+
+            diskD.Add(jpgFile);
+            diskD.Add(docFile);
+            fileSystem.Add(diskD);
+
+            fileSystem.Print();
+            Console.WriteLine();
+           
+            diskD.Remove(jpgFile);
+
+            Component docsFolder = new Directory("Мои Документы");
+            Component txtFile = new File("readme.txt");
+            Component csFile = new File("Lab_2.cs");
+            docsFolder.Add(txtFile);
+            docsFolder.Add(csFile);
+            diskD.Add(docsFolder);
+
+            fileSystem.Print();
+
+            Console.Read();
         }
     }
 }
