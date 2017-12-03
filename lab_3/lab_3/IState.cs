43108aa4369a01aa315260a14cf2d3006197f239
Ksenia <Ksenia@DESKTOP-FPNQ7IM>
2017-12-03 21:39:20 +0200

    Create IState

---
+++ lab_3/lab_3/IState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    interface IState
+    {
+        void Create();      // створити
+        void Consider();    // розглянути
+        void Defer();       // відкласти
+        void Reject();      // відхилити
+        void Confirm();     // підтвердити        
+        void Withdraw();    // відкликати        
+    }
+}
